c6ea60940ad3d75b51853e519f803080e2252b6c
Oleg Gulitsky <oleg.gulitsky@gmail.com>
2019-12-29 11:42:18 +0300

    App renaming.

--- android/app/src/main/java/com/radioplatonew/MainActivity.java
+++ android/app/src/main/java/com/radioplatonew/MainActivity.java
@@ -1,4 +1,4 @@
-package com.radioplatonew;
+package com.radioplato;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RadioPlatoNew";
+    return "RadioPlato";
   }
 }
--- android/app/src/main/java/com/radioplatonew/MainApplication.java
+++ android/app/src/main/java/com/radioplatonew/MainApplication.java
@@ -1,4 +1,4 @@
-package com.radioplatonew;
+package com.radioplato;
 
 import android.app.Application;
 import android.content.Context;
